7161776e193172944356241ad48143a1ddd61bd4
João Vitor Nunes da Silva <135175090+Joao-Vitor-Nunes@users.noreply.github.com>
2024-06-28 18:54:49 -0400

    att
    att

--- index.js
+++ index.js
@@ -1,71 +1,60 @@
-class ORM {
-  constructor(){
-      if (this.constructor == ORM){
-          throw new Error("Classe abstrata não pode ser instanciada diretamente.")
-      }
+import ORM from "./orm.js";
 
-      this.array = [];
-      this.counter = 0;
-  }
-
-  /*SELECIONAR*/
-  selecionar(id) {
-      console.log("===================================")
-      for (let i = 0; i < this.array.length; i++) {
-          if (this.array[i].id == id) {
-              return `Objeto SELECIONADO com sucesso: ${JSON.stringify(this.array[i], null, 2)}\n`;
-          }
-      }
-      return "Não foi possível SELECIONAR o objeto\n";
-  }
-
-  /*SALVAR*/
-  salvar(object){
-      console.log("===================================")
-      console.log('Objeto SALVO com sucesso')
-      object.id = this.counter;
-      this.array.push(object);
-      this.counter++;
-      return object;
-  }
-
-  /*ATUALIZAR*/
-  atualizar(id, object){
-      console.log("===================================")
-      for(let i = 0; i < this.array.length; i++){
-          if(this.array[i].id == id){
-              object.id = id;
-              this.array[i] = object;
-              return `Objeto ATUALIZADO com sucesso: ${JSON.stringify(this.array[i], null, 2)}\n`;
-          }
-      }
-      return "Não foi possível ATUALIZAR o objeto";
-  }    
-  
-
-  /*LISTAR*/
-  listar() {
-      console.log("===================================")
-      console.log('Objetos LISTADOS')
-      const descriptions = this.array.map(object => 
-          `tamanho=${object.tamanho}, peso=${object.peso}, altura=${object.altura}, id=${object.id}`
-      );
-      return descriptions;
-  }
-
-  /*DELETAR*/
-  deletar(id) {
-      console.log("===================================")
-      for (let i = 0; i < this.array.length; i++) {
-          if (this.array[i].id == id) {
-              let objDeletado = this.array[i];
-              this.array.splice(i, 1);
-              return `Objeto DELETADO com sucesso: ${JSON.stringify(objDeletado, null, 2)}\n`;
-          }
-      }
-      return "Não foi possível DELETAR o objeto";
-  }
-
+class AAA extends ORM{
+    constructor(){
+        super();
+    }
 }
 
-export default ORM
+const objeto = new AAA()
+
+/*salvar*/
+console.log(
+objeto.salvar({tamanho: "quatro", peso:"três", altura:4}));
+console.log("");
+
+console.log(
+objeto.salvar({tamanho: 5, peso:10, altura:10}));
+console.log("");
+
+console.log(
+objeto.salvar({tamanho: 5, peso:10, altura:10}));
+console.log("");
+
+console.log(
+objeto.salvar({tamanho: 5, peso:10, altura:10}));
+console.log("");
+
+console.log(
+objeto.salvar({tamanho: 5, peso:10, altura:10}));
+console.log("");
+
+/*selecionar*/
+console.log(
+objeto.selecionar(1));
+console.log("")
+
+/*alterar*/
+console.log(
+    objeto.atualizar(2, {tamanho: "quatro", peso:"três", altura:4})
+)
+
+/*listar*/
+console.log(
+    objeto.listar()
+);
+console.log("")
+
+/*deletar*/
+console.log(objeto.deletar(1)); // Adiciona log para verificar retorno
+console.log("")
+
+/*listar novamente para verificar a exclusão*/
+console.log(
+    objeto.listar()
+);
+console.log("")
+
+console.log(
+    
+)
